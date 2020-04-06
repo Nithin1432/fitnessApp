2974728be70b6f362e5c789c0ce84e631a16089e
Nithin1423 <Julakann1@hawkmail.newpaltz.edu>
2020-04-07 01:36:46 +0530

    refactored front end code

---
+++ client/src/router/index.js
@@ -0,0 +1,28 @@
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue';
+import Profile from '../views/Profile.vue';
+import ExerciseLog from '../views/ExerciseLog.vue';
+import Exercise from '../views/Exercise.vue';
+import About from '../views/About.vue';
+import Home from '../views/Home.vue';
+import Suggestion from '../views/Suggestion.vue';
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/login', component: Login },
+  { path: '/profile', component: Profile },
+  { path: '/exercise-log', component: ExerciseLog },
+  { path: '/exercise', component: Exercise },
+  { path: '/about', component: About },
+  { path: '/home', component: Home },
+  { path: '/suggestion', component: Suggestion },
+];
+
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+  });
+  
+
+export default router
